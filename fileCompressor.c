3ed20ac984bd3d711dff26a7c6b68657d96918aa
Shivam Patel <46139029+Shivam1615@users.noreply.github.com>
2019-03-27 16:15:06 -0400

    Update fileCompressor.c

--- fileCompressor.c
+++ fileCompressor.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<dirent.h>
 
+int main(int argc, char **argv){
+
+DIR *directory = opendir(".");
+struct dirent *directories;
+
+if(directory){
+
+while((directories = readdir(directory)) != NULL){
+        printf("%s\n", directories->d_name);
+}
+close(directory);
+}
+
+return 0;
+}
