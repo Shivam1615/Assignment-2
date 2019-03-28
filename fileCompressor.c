b4f26165cdde5a4afb1ea0b334662be7bf473f15
DhanushSG <48163624+DhanushSG@users.noreply.github.com>
2019-03-28 13:25:00 -0400

    Update fileCompressor.c

--- fileCompressor.c
+++ fileCompressor.c
@@ -1,19 +1,44 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<dirent.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
 
-int main(int argc, char **argv){
+void printdir(char *dir,int depth)
+{
+    DIR *d;
+    struct dirent *entry;
+    struct stat check;
+    int spaces=depth*4;
 
-DIR *directory = opendir(".");
-struct dirent *directories;
-
-if(directory){
-
-while((directories = readdir(directory)) != NULL){
-        printf("%s\n", directories->d_name);
+    if((d = opendir(dir)) == NULL) {
+        fprintf(stderr,"cannot open directory: %s\n", dir);
+        return;
+    }
+    chdir(dir);
+    while((entry = readdir(d)) != NULL) {
+        lstat(entry->d_name,&check);
+        if(S_ISDIR(check.st_mode)) {
+            
+            if(strcmp(".",entry->d_name) == 0 ||
+                strcmp("..",entry->d_name) == 0)
+                continue;
+            printf("%*s%s/\n",spaces,"",entry->d_name);
+            
+            printdir(entry->d_name,depth+1);
+        }
+        else printf("%*s%s\n",spaces,"",entry->d_name);
+    }
+    chdir("..");
+    closedir(d);
 }
-closedir(directory);
-}
 
-return 0;
+int main(int argc, char* argv[])
+{
+        char *currentDir, x[2]=".";
+        currentDir=x;
+
+        printdir(currentDir,0);
+
+        return 0;
 }
