a46b24bf5073db452858b5b2cf042d3cc07c1eef
DhanushSG <48163624+DhanushSG@users.noreply.github.com>
2019-03-29 14:40:25 -0400

    Update fileCompressor.c

--- fileCompressor.c
+++ fileCompressor.c
@@ -3,6 +3,8 @@
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 void printdir(char *dir)
 {
@@ -18,15 +20,24 @@ void printdir(char *dir)
     while((entry = readdir(d)) != NULL) {
         lstat(entry->d_name,&check);
         if(S_ISDIR(check.st_mode)) {
-            
-            if(strcmp(".",entry->d_name) == 0 ||
-                strcmp("..",entry->d_name) == 0)
+
+            if(strcmp(".",entry->d_name) == 0 || strcmp("..",entry->d_name) == 0)
                 continue;
             printf("%s/",entry->d_name);
-            
+
             printdir(entry->d_name);
         }
-        else printf("%s\n",entry->d_name);
+        else {
+                if( strcmp("fileCompressor.c",entry->d_name)==0 || strcmp("Makefile",entry->d_name)==0 || strcmp("fileCompressor",entry->d_name)==0)
+                        continue;
+                printf("%s contains: ",entry->d_name);
+                char c;
+                FILE *pf=fopen(entry->d_name,"r");
+                while((c=fgetc(pf))!=EOF){
+                        printf("%c",c);
+                }
+                fclose(pf);
+        }
     }
     chdir("..");
     closedir(d);
