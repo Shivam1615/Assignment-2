506de4c7943529fc983bbe96bebcecffc363ff11
DhanushSG <48163624+DhanushSG@users.noreply.github.com>
2019-03-29 15:42:58 -0400

    Update fileCompressor.c

--- fileCompressor.c
+++ fileCompressor.c
@@ -1,13 +1,40 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
 
-void printdir(char *dir)
+typedef struct data{
+        char word;
+        int freq;
+} data;
+
+int size=0;
+
+void countFreq(FILE *pf, data* frequencies){
+
+        char c;
+        while((c=fgetc(pf))!=EOF){
+                printf("%c",c);
+
+
+
+
+
+
+        }
+        fclose(pf);
+        return;
+}
+
+void findFiles(char *dir)
 {
+
+data *frequencies=(data*)malloc(sizeof(data)*20);
+
     DIR *d;
     struct dirent *entry;
     struct stat check;
@@ -20,35 +47,30 @@ void printdir(char *dir)
     while((entry = readdir(d)) != NULL) {
         lstat(entry->d_name,&check);
         if(S_ISDIR(check.st_mode)) {
-
             if(strcmp(".",entry->d_name) == 0 || strcmp("..",entry->d_name) == 0)
                 continue;
             printf("%s/",entry->d_name);
-
-            printdir(entry->d_name);
+            findFiles(entry->d_name);
         }
         else {
                 if( strcmp("fileCompressor.c",entry->d_name)==0 || strcmp("Makefile",entry->d_name)==0 || strcmp("fileCompressor",entry->d_name)==0)
                         continue;
                 printf("%s contains: ",entry->d_name);
-                char c;
                 FILE *pf=fopen(entry->d_name,"r");
-                while((c=fgetc(pf))!=EOF){
-                        printf("%c",c);
-                }
-                fclose(pf);
+                countFreq(pf,frequencies);
         }
     }
     chdir("..");
     closedir(d);
 }
 
+
 int main(int argc, char* argv[])
 {
         char *currentDir, x[2]=".";
         currentDir=x;
 
-        printdir(currentDir);
+        findFiles(currentDir);
 
         return 0;
 }
